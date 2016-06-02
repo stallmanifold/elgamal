c1bb23a722145bd9643064d35e6824b9b1d58c1e
Stallmanifold <stallmanifold@gmail.com>
2016-06-02 04:02:55 -0500

    Add implmentation of ModPow for BigInt.

--- src/modpow.rs
+++ src/modpow.rs
@@ -1,31 +1,71 @@
 use num::{BigInt, Integer, Zero, One, Num};
+//use std::ops::Mul;
 
-pub trait ModPow<I> {
-    fn mod_pow(base: &I, exponent: &I, modulus: &I) -> I;
+
+pub trait ModPow {
+    type IntType;
+
+    fn mod_pow(base: &Self::IntType, exponent: &Self::IntType, modulus: &Self::IntType) -> Self::IntType;
 }
 
-impl<I> ModPow<I> for I where I: Integer + Num {
-    fn mod_pow(base: &I, exponent: &I, modulus: &I) -> I {
-        let zero: I = Zero::zero();
-        let one: I  = One::one();
-        let two: I  = one + one;
+/*
+impl<T> ModPow for T where T: Num + Integer + Clone + Mul<&'a T> {
+    type IntType = T;
 
-        assert!(modulus != &zero);
+    fn mod_pow(base: &T, exponent: &T, modulus: &T) -> T {
+        let zero: T = Zero::zero();
+
+        assert!(*modulus != zero);
+
+        let one: T = <T as One>::one();
+        let two: T = <T as One>::one() + <T as One>::one();
 
         if *modulus == one {
-            return Zero::zero();
-        }       
+            return zero;
+        }
 
-        let mut result = one;
-        let mut modded_base = base.mod_floor(modulus);
-        let mut divided_exponent = exponent;
+        let mut result: T = <T as One>::one();
+        let mut modded_base: T = base.mod_floor(modulus);
+        let mut divided_exponent: T = exponent.clone();
         
-        while divided_exponent > &zero {
+        while divided_exponent > zero {
             if divided_exponent.mod_floor(&two) == one {
                 result = (result * modded_base).mod_floor(modulus);
             }
-            divided_exponent = &divided_exponent.div_floor(&two);
+            divided_exponent = divided_exponent.div_floor(&two);
             modded_base = (modded_base * modded_base).mod_floor(modulus);
+        }
+
+        result
+    }
+}
+*/
+
+impl ModPow for BigInt {
+    type IntType = BigInt;
+
+    fn mod_pow(base: &BigInt, exponent: &BigInt, modulus: &BigInt) -> BigInt {
+        let zero: BigInt = Zero::zero();
+
+        assert!(*modulus != zero);
+
+        let one: BigInt = One::one();
+        let two: BigInt = <BigInt as One>::one() + <BigInt as One>::one();
+
+        if *modulus == one {
+            return zero;
+        }
+
+        let mut result: BigInt = One::one();
+        let mut modded_base: BigInt = base.mod_floor(modulus);
+        let mut divided_exponent: BigInt = exponent.clone();
+        
+        while divided_exponent > zero {
+            if divided_exponent.mod_floor(&two) == one {
+                result = (result * &modded_base).mod_floor(modulus);
+            }
+            divided_exponent = divided_exponent.div_floor(&two);
+            modded_base = (&modded_base * &modded_base).mod_floor(modulus);
         }
 
         result
