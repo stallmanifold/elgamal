4e0b9e6a85c2d2e6e506ddae4f914794316bc5ac
Stallmanifold <stallmanifold@gmail.com>
2016-07-13 23:29:07 -0500

    Remove Generalized El Gamal.

--- src/generalized_elgamal.rs
+++ src/generalized_elgamal.rs
@@ -3,6 +3,10 @@ use rand::Rng;
 use modal::ModExp;
 
 
+//pub type CipherText = Vec<(BigInt, BigInt)>;
+pub type PlainText  = Vec<u8>;
+
+
 pub trait GroupElem: Num + Integer + Zero + One + Clone {}
 
 #[derive(Clone)]
@@ -20,10 +24,12 @@ impl<Elem> GroupSpec<Elem> where Elem: GroupElem {
     }
 }
 
+// TODO: Add some other traits to give the appearance of a cyclic group.
 pub trait CyclicGroup {
     type Elem: GroupElem + ModExp;
 
     fn group_spec(&self) -> GroupSpec<Self::Elem>;
+    fn bit_size(&self) -> usize;
 }
 
 #[derive(Clone)]
@@ -38,6 +44,14 @@ impl<G> PublicKey<G> where G: Clone + CyclicGroup {
             group_spec: group_spec.clone(),
             key: key.clone(),
         }
+    }
+
+    pub fn modulus(&self) -> &G::Elem {
+        &self.group_spec.modulus
+    }
+
+    pub fn gen(&self) -> &G::Elem {
+        &self.group_spec.gen
     }
 }
 
@@ -98,7 +112,7 @@ pub trait GenerateKeyPair<G> where G: Clone + CyclicGroup {
 
 pub fn generate<R, G>(rng: &mut R, spec: &GroupSpec<G::Elem>) -> KeyPair<G> 
     where R: Rng,
-          G: Clone + CyclicGroup + RandGroupElem<R, G>,
+          G: Clone + CyclicGroup + RandGroupElem<R, G>
 {
     let lbound: G::Elem = <G::Elem as One>::one();
     let x: G::Elem = <G as RandGroupElem<_,_>>::gen_group_elem_range(rng, &lbound, &spec.modulus);
@@ -108,4 +122,39 @@ pub fn generate<R, G>(rng: &mut R, spec: &GroupSpec<G::Elem>) -> KeyPair<G>
     let public_key  = PublicKey::new(spec, &h);
 
     KeyPair::new(private_key, public_key)
+}
+
+#[inline]
+fn encrypt_digit<G>(digit: &G::Elem, nonce: &G::Elem, key: &PublicKey<G>) -> (G::Elem, G::Elem) 
+    where G: Clone + CyclicGroup
+{      
+    let gamma = <G::Elem as ModExp>::mod_exp(key.gen(), &nonce, key.modulus());
+        
+    let mmp = digit.mod_floor(key.modulus());
+    let ak  = <G::Elem as ModExp>::mod_exp(&key.key, &nonce, key.modulus());
+    let delta = Integer::mod_floor(&(mmp*ak), key.modulus());
+
+    (gamma, delta)
+}
+
+pub fn encrypt<R, G>(rng: &mut R, plain_text: &[u8], key: &PublicKey<G>) -> CipherText 
+    where R: Rng,
+          G: Clone + CyclicGroup + RandGroupElem<R, G>
+{
+    let lbound = One::one();
+    let ubound = &key.modulus() - One::one();
+    let nonce  = <G::Elem as RandGroupElem<_,_>>::gen_group_elem_range(rng, &lbound, &ubound);
+
+    let bits_per_digit  = key.group_spec.bits_size() - 1;
+    let bytes_per_digit = bits_per_digit / 8;
+
+    let mut digits  = Vec::new();
+
+    for chunk in plain_text.chunks(bytes_per_digit) {
+        let digit = encode(chunk);
+        let (gamma, delta) = encrypt_digit(&digit, &nonce, key);
+        digits.push((gamma, delta));
+    }
+
+    digits
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@ extern crate crypto;
 extern crate rustc_serialize;
 
 pub mod elgamal;
-pub mod generalized_elgamal;
 
 #[cfg(test)]
 mod tests;
